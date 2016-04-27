699ef471c8b3da561e5616a588a880eb283d1419
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-04-26 22:40:37 -0400

    chore(example): added basic example

---
+++ examples/basic/index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+function add (a, b) {
+  return a + b
+}
+
+function notUsed () { // eslint-disable-line no-unused-vars
+  console.log('this function is not used')
+}
+
+// load another js file just for fun
+require('./foo')
+
+const N = parseInt(process.argv[2]) || 2
+console.log('this file runs for %d seconds', N)
+console.log('every second it adds two numbers')
+
+var k = 0
+const s = setInterval(function () {
+  const a = parseInt(Math.random() * 10)
+  const b = parseInt(Math.random() * 10)
+  console.log('%d + %d = %d', a, b, add(a, b))
+  console.log('%d / %d done', k + 1, N)
+  k += 1
+  if (k >= N) {
+    clearInterval(s)
+    process.exit(0)
+  }
+}, 2000)
--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-'use strict'
+console.log('real time code coverage')
+const glob = require('glob')
+const path = require('path')
+const jsFiles = glob.sync('**/*.js')
+  .map((name) => path.resolve(name))
+console.log('preparing for possible coverage of %d source js files', jsFiles.length)
+
+var cover
+
+Object.defineProperty(global, '__coverage__', {
+  configurable: true,
+  enumerable: true,
+  get: () => {
+    // console.log('getting coverage')
+    // console.log(cover)
+    return cover
+  },
+  set: (value) => {
+    console.log('setting new coverage object')
+    // console.log(value)
+    // happens once
+    // prepare for every source file ;)
+    jsFiles.forEach((filename) => {
+      var fileCoverage
+      // usually each file coverage object is something like
+      // { path: '/Users/gleb/git/training/node/test-nyc-require/foo.js',
+      //     s: { '1': 1 },
+      //     b: {},
+      //     f: {},
+      //     fnMap: {},
+      //     statementMap: { '1': [Object] },
+      //     branchMap: {} } }
+      Object.defineProperty(value, filename, {
+        configurable: true,
+        enumerable: true,
+        get: () => fileCoverage,
+        set: (coverage) => {
+          console.log('set file coverage for', filename)
+          fileCoverage = coverage
+        }
+      })
+    })
+    cover = value
+  }
+})
