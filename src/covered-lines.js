32f8c5c6a695911f259d5089065ed01b88249714
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-04-27 07:33:49 -0400

    chore(lines): only using start line for statement

--- src/covered-lines.js
+++ src/covered-lines.js
@@ -2,7 +2,7 @@
 
 const la = require('lazy-ass')
 const is = require('check-more-types')
-const debug = require('debug')('live')
+// const debug = require('debug')('live')
 const summarizeFileCoverage = require('istanbul').utils.summarizeFileCoverage
 la(is.fn(summarizeFileCoverage), 'not a function', summarizeFileCoverage)
 
@@ -11,6 +11,7 @@ la(is.fn(summarizeFileCoverage), 'not a function', summarizeFileCoverage)
   '1': { start: [Object], end: [Object] },
   '2': { start: [Object], end: [Object] }
 }
+only use the start line?
 */
 function getLinesForStatement (statementMap, statement) {
   la(is.unemptyString(statement), 'invalid statement index', statement)
@@ -22,19 +23,10 @@ function getLinesForStatement (statementMap, statement) {
   la(is.number(endLine), 'invalid end', map.end, 'for', statement)
   la(startLine <= endLine, 'start should be before end', map.start, map.end)
 
-  if (startLine === endLine) {
-    return [startLine]
-  }
-
-  debug('lines covered for statement', statement, 'from', startLine, 'to', endLine)
-
-  var k = 0
-  var lines = []
-  for (k = startLine; k < endLine; k += 1) {
-    lines.push([k])
-  }
-  return lines
+  return [startLine]
 }
+
+la(is.fn(getLinesForStatement))
 
 /*
 outputs an object like
@@ -52,30 +44,24 @@ function computeLineCoverage (fileCoverage) {
   la(is.object(fileCoverage.s), 'missing statements', fileCoverage)
   la(is.object(fileCoverage.statementMap), 'missing statement map', fileCoverage)
 
-  const summary = summarizeFileCoverage(fileCoverage)
-  const lines = summary.linesCovered
-  la(is.object(lines), 'could not compute line coverage from', fileCoverage, summary)
+  // const summary = summarizeFileCoverage(fileCoverage)
+  // const lines = summary.linesCovered
+  // la(is.object(lines), 'could not compute line coverage from', fileCoverage, summary)
 
-  la(is.fn(getLinesForStatement))
+  const lines = {}
+  Object.keys(fileCoverage.s).forEach((s, k) => {
+    const statementCounter = fileCoverage.s[s]
+    const statementLines = getLinesForStatement(fileCoverage.statementMap, s)
+    la(is.array(statementLines), 'no lines for statement', s)
+    // debug('statement', s, 'index', k, 'counter', statementCounter)
 
-  // const lines = {}
-  // Object.keys(fileCoverage.s).forEach((s, k) => {
-  //   if (k > 3) {
-  //     return
-  //   }
-  //   const statementCounter = fileCoverage.s[s]
-  //   const statementLines = getLinesForStatement(fileCoverage.statementMap, s)
-  //   la(is.array(statementLines), 'no lines for statement', s)
-  //   debug('statement', s, 'index', k, 'counter', statementCounter)
-
-  //   statementLines.forEach((line) => {
-  //     if (!(line in lines)) {
-  //       lines[line] = statementCounter
-  //     } else {
-  //       lines[line] += statementCounter
-  //     }
-  //   })
-  // })
+    statementLines.forEach((line) => {
+      const prevValue = line[lines]
+      if (typeof prevValue === 'undefined' || prevValue < statementCounter) {
+        lines[line] = statementCounter
+      }
+    })
+  })
 
   return lines
 }
